17e65dd2451a09d302a1276b479b6f17458e1ba3
SUJITH P JOSE <sujithpjose@CUSTs-MacBook-Air.local>
2019-04-08 16:13:05 +0530

    summary added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,13 +4,13 @@ const forecast = (lat, lng, callback) => {
     // https://api.darksky.net/forecast/8a4b7a7ca5c653159d368a605598373e/37.8267,-122.4233?exclude=currently,minutely,flags
     const url = `https://api.darksky.net/forecast/8a4b7a7ca5c653159d368a605598373e/${lat},${lng}`;
 
-    request({ url, json: true }, (err,{ body }) => {
+    request({ url, json: true }, (err, { body }) => {
         if (err) {
             callback('Unable to connect to server')
         } else if (body.error) {
             callback(body.error)
         } else {
-            const message = `Current temperature is ${body.currently.temperature} and there is ${body.currently.precipProbability} chance for rain`;
+            const message = `Current temperature is ${body.currently.temperature} and there is ${body.currently.precipProbability} chance for rain. Summary : ${body.hourly.summary}`;
             callback(undefined, message);
         }
     })
