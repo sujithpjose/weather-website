f7427393f06a2a04f0d6254dc5a13a938aaa2a0d
SUJITH P JOSE <sujithpjose@CUSTs-MacBook-Air.local>
2019-04-08 15:02:50 +0530

    =console msg removed

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client side js loaded');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message_one');
